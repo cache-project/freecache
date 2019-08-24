2263729f3a2aa06be1e26a124b6320c38dba3cbd
Piper McCorkle <contact@piperswe.me>
2019-08-24 14:20:59 -0700

    Add stylesheet for markdown

--- meta/builder/transform/markdown.py
+++ meta/builder/transform/markdown.py
@@ -1,12 +1,14 @@
 import os
 import mistletoe
 
+header = '<link rel="stylesheet" href="/freecache/runtime/style.css" />'
+
 
 def transform(meta_dict, input_file, output_file):
   output_file_html = output_file.replace('.md', '.html')
 
   with open(output_file_html, 'w') as o_f:
     with open(input_file, 'r') as i_f:
-      o_f.write(mistletoe.markdown(i_f.read()))
+      o_f.write(mistletoe.markdown(header + i_f.read()))
 
   return os.path.basename(output_file_html)
